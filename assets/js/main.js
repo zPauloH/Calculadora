94d2832ce9d814d4a69c26e14018c1e71b71ceda
zPauloH <paulohenrique23477@gmail.com>
2022-03-15 11:04:14 -0300

    calculadora

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+class Calculadora {
+    constructor() {
+        this.display = document.querySelector('.display');
+
+
+        this.inicia = () => {
+            this.capturaCliques();
+            this.capturaEnter();
+        };
+
+        this.capturaEnter = () => {
+            this.display.addEventListener('keypress', e => {
+                if (e.keyCode === 13) {
+                    this.realizaConta();
+                }
+            });
+        };
+
+        this.capturaCliques = () => {
+            document.addEventListener('click', event => {
+                const el = event.target;
+                if (el.classList.contains('btn-num'))
+                    this.addNumDisplay(el);
+                if (el.classList.contains('btn-clear'))
+                    this.clear(el);
+                if (el.classList.contains('btn-del'))
+                    this.del(el);
+                if (el.classList.contains('btn-eq'))
+                    this.eq(el);
+            });
+        };
+
+        this.realizaConta = () => {
+            try {
+                const conta = eval(this.display.value);
+                if (!conta) {
+                    alert('conta invalida');
+                    return;
+                }
+                this.display.value = conta;
+            } catch (e) {
+                alert('Conta invalida');
+                return;
+            }
+        };
+
+        this.addNumDisplay = el => {
+            this.display.value += el.innerText;
+            this.display.focus();
+        };
+        this.clear = () => this.display.value = '';
+        this.del = () => this.display.value = this.display.value.slice(0, -1);
+
+    }
+}
+const calculadora = new Calculadora();
+calculadora.inicia();
